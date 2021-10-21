877710ffd9089f08bbfbc6a23d46a3ee159f2e2b
ranets <mymailis106@gmail.com>
2021-10-21 06:40:47 +0300

    upd lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,16 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	size_t len = strlen(str);
+
+	char tmp;
+	for (size_t  i = 0; i < len/2; i++) 
+	{
+		tmp = str[len - i - 1];
+		str[len - i - 1] = str[i];
+		str[i] = tmp;
+	}
 }
 
